b2772e53ceb88f862edd7822c771182025f64515
Leo <leonar2zb@gmail.com>
2025-05-17 17:38:46 -0300

    + Established model relationship User-Role

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Role extends Model
 {
     //
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
 }
